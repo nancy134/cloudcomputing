f72aba43eb49d313f7d892d77fc835fb41dfd22c
Michael D Pastrone <mdpastrone@outlook.com>
2018-07-30 13:59:33 -0400

    Parts

---
+++ parts1.js
@@ -0,0 +1,24 @@
+function getAllParts(){
+  console.log("getAllParts()");
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      var responseJSON = JSON.parse(this.responseText);
+      var rows = "";
+      for (i=0; i<responseJSON.data.length; i++){
+        rows += "<tr><td>"+responseJSON.data[i].id+"</td>"+
+        "<td>"+responseJSON.data[i].name+"</td></tr>";
+        console.log(responseJSON.data[i].id+" "+responseJSON.data[i].name);
+      }
+      console.log("rows:"+rows);
+      var partsTable = document.getElementById("partsTable").innerHTML;
+      console.log("partsTable: "+partsTable);
+      var partsRows = partsTable + rows;
+      console.log("partsRows: "+partsRows);
+      partsTable.innerHTML = partsRows;
+    }
+  };
+  xhttp.open("GET","http://ec2-18-222-141-0.us-east-2.compute.amazonaws.com/rest/api/parts", true);
+  xhttp.send();
+}
+
